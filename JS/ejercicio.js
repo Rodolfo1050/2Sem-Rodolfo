0abe7cbaa77c8db4b3855d09fbaaeeb5cb18b743
Rodolfo <rodolfo105@outlook.com>
2019-05-13 13:11:51 -0500

    version-1

---
+++ JS/ejercicio.js
@@ -0,0 +1,5 @@
+document.getElementById("cero").innerHTML = "Bienvenido";
+
+var x = document.getElementById("uno").innerHTML = "Esto es una prueba";
+
+document.getElementById("dos").innerHTML = x;
